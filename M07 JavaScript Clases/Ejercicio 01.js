46bf2aff7b942fa871d87f28dcd026fe7ee3e8f6
alemezio <mezio@ifir-conicet.gov.ar>
2023-09-05 11:30:36 -0300

    M07 homework

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -10,18 +10,44 @@ function crearUsuario() {
    // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
    // Retornar la clase.
    // Tu código:
+   class Usuario{
+      constructor(usuario,nombre,email,password){
+         this.usuario=usuario;
+         this.nombre=nombre;
+         this.email=email;
+         this.password=password;
+      }
+      saludar(){
+         return 'Hola, mi nombre es '+this.nombre;
+      }
+   };
+   return Usuario
 }
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+      return 'Hello World!'
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function(){
+      var arrayOriginal = this.split('');
+      var arrayInvertido=[];
+      for (let i=0; i<arrayOriginal.length; i++){
+         // arrayInvertido[i]=arrayOriginal[arrayOriginal.length-1-i];
+         arrayInvertido.push(arrayOriginal[arrayOriginal.length-1-i]);
+         // ANDAN LAS DOS FORMAS
+      }
+      var invertido = arrayInvertido.join('');
+      return invertido
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
